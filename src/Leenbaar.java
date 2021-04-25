1ff29a691c6668dcc23599336c0db316948169ed
Sjoerd <sbeetsma99@gmail.com>
2021-04-25 15:08:37 +0200

    Add interface voor leenbare objecten

---
+++ src/Leenbaar.java
@@ -0,0 +1,21 @@
+public interface Leenbaar {
+
+    String getTitel();
+
+    void setTitel(String Titel);
+
+    String getBeschrijving();
+
+    void setBeschrijving(String Beschrijving);
+
+    boolean getBeschikbaar();
+
+    void setBeschikbaar(boolean Beschikbaar);
+
+    void Huren();
+
+    void Inleveren();
+
+    String toString();
+
+}
