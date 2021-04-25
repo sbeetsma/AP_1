624a87c696627ffbf2497a72fab6690dfb206efb
Sjoerd <sbeetsma99@gmail.com>
2021-04-25 15:11:36 +0200

    Add object EBoek (extends Boek)

---
+++ src/EBoek.java
@@ -0,0 +1,10 @@
+public class EBoek extends Boek {
+
+    public EBoek(String Titel, int Paginas, int UitgaveJaar) {
+        super(Titel, Paginas, UitgaveJaar);
+    }
+    // Eboek huren veranderd niet beschikbaarheid
+    public void Huren() {
+        if(!getBeschikbaar()) {System.out.println("Niet Beschikbaar!");}
+    }
+}
